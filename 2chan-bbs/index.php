67ddbea7e556608273315c51ffc6182fd01c43aa
risarisato <88628553+risarisato@users.noreply.github.com>
2023-08-19 13:30:51 +0900

    初期ファイルを追加

---
+++ 2chan-bbs/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP掲示板スレッド機能</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1 class="title">PHP掲示板スレッド機能-掲示板</h1>
+        <!-- 「CTRL」+「/」でhtmlのコメントアウトできる -->
+        <!-- hr は水平線 -->
+        <!-- http://localhost:8080/2chan-bbs/ -->
+        <hr>
+    </header>
+
+    <!-- スレッドエリア -->
+    <div class="threadWrapper">
+        <div class="childWrapper">
+            <div class="threadTitle">
+                <span>【スレッド掲示板作成】</span>
+                <h1>PHP掲示板スレッド機能-掲示板を作ってみた</h1>
+            </div>
+            <section>
+                <article>
+                    <div class="wrapper">
+                        <div class="nameArea">
+                            <span>名前：</span>
+                            <p class="username">Shincode</p>
+                            <time>:2023/08/19 13:02</time>
+                        </div>
+                        <p>手書きのハードコーディングです</p>
+                    </div>
+                </article>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
