01ca9ad10d55f3ee534795823996f422af287c5b
T.Aoki_notepc <nqg44226@gmail.com>
2023-08-27 16:41:35 +0900

    ヘッダーや関数や投稿取得・表示のフォルダを分けた

---
+++ app/parts/validation.php
@@ -0,0 +1,8 @@
+<!-- バリデーションチェックのエラーを表示させる  -->
+<?php if (isset($error_message)) : ?>
+    <ul class = "errorMessage">
+        <?php foreach ($error_message as $error) : ?>
+            <li><?php echo $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
